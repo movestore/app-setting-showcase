c084e9b8ad4b362ac463e52b3480b619078b1596
Julian Hirt <julian@couchbits.com>
2020-02-19 11:44:41 +0100

    Add test app

---
+++ RFunction.R
@@ -0,0 +1,12 @@
+library('move')
+
+rFunction = function(testTimestamp,testNumber,testString,testDropdown,testRadioButtons,testCheckbox, data) {
+  print(paste("testTimestamp Value: ", testTimestamp))
+  print(paste("testNumber Value: ", testNumber))
+  print(paste("testString Value: ", testString))
+  print(paste("testDropdown Value: ", testDropdown))
+  print(paste("testRadioButtons Value: ", testRadioButtons))
+  print(paste("testCheckbox Value: ", testCheckbox))
+
+  data
+}
