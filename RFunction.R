31585278f21a03ea12d9003005c9df4c2ccd173d
Julian Hirt <julian@couchbits.com>
2020-08-05 15:39:49 +0200

    Add test decimal

--- RFunction.R
+++ RFunction.R
@@ -1,12 +1,13 @@
 library('move')
 
-rFunction = function(testTimestamp,testNumber,testString,testDropdown,testRadioButtons,testCheckbox, data) {
+rFunction = function(testTimestamp,testNumber,testString,testDropdown,testRadioButtons,testCheckbox, testDecimal, data) {
   print(paste("testTimestamp Value: ", testTimestamp))
   print(paste("testNumber Value: ", testNumber))
   print(paste("testString Value: ", testString))
   print(paste("testDropdown Value: ", testDropdown))
   print(paste("testRadioButtons Value: ", testRadioButtons))
   print(paste("testCheckbox Value: ", testCheckbox))
+  print(paste("testDecimal value: ", testDecimal))
 
   data
 }
