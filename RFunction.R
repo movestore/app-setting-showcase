c6b62faa9f8a80c30af15d8551c0529846741776
Julian Hirt <julian@couchbits.com>
2020-08-06 17:06:04 +0200

    Renmae decimal to double setting type

--- RFunction.R
+++ RFunction.R
@@ -1,13 +1,13 @@
 library('move')
 
-rFunction = function(testTimestamp,testNumber,testString,testDropdown,testRadioButtons,testCheckbox, testDecimal, data) {
+rFunction = function(testTimestamp,testNumber,testString,testDropdown,testRadioButtons,testCheckbox, testDouble, data) {
   print(paste("testTimestamp Value: ", testTimestamp))
   print(paste("testNumber Value: ", testNumber))
   print(paste("testString Value: ", testString))
   print(paste("testDropdown Value: ", testDropdown))
   print(paste("testRadioButtons Value: ", testRadioButtons))
   print(paste("testCheckbox Value: ", testCheckbox))
-  print(paste("testDecimal value: ", testDecimal))
+  print(paste("testDouble value: ", testDouble))
 
   data
 }
