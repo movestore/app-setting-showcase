35b8f826cbdad467cb2eedc5c14dd7dd0457ec8d
Clemens Hahn <clemens@couchbits.com>
2024-03-05 09:26:35 +0100

    add app-setting of type `USER_FILE`

--- RFunction.R
+++ RFunction.R
@@ -9,6 +9,7 @@ rFunction = function(
     testCheckbox,
     testDouble,
     testLocalFile,
+    testUserFile,
     forceError,
     data
 ) {
@@ -20,6 +21,13 @@ rFunction = function(
   printArg("testCheckbox", testCheckbox)
   printArg("testDouble", testDouble)
   printArg("testLocalFile", testLocalFile)
+  printArg("testUserFile", testUserFile)
+
+  localFile <- paste0(getAppFilePath("testLocalFile", fallback=FALSE), "expected.txt")
+  printFileInfo("testLocalFile", localFile)
+
+  userFile <- paste0(getAppFilePath("testUserFile", fallback=FALSE), testUserFile)
+  printFileInfo("testUserFile", userFile)
 
   if (forceError) {
     logger.warn("I will now force an error (as you requested!)")
@@ -31,4 +39,9 @@ rFunction = function(
 printArg <- function(name, value) {
   v <- if (is.null(value)) 'null-value' else value
   logger.info("%s: '%s'", name, v)
+}
+
+printFileInfo <- function(settingId, file) {
+  info <- file.info(file)
+  logger.info("%s: %s", settingId, toString(info))
 }
